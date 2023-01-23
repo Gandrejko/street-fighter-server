310692844c1ab53998b1f51105060bed9a0e4235
Andrew <gandrejko2002@gmail.com>
2023-01-23 14:34:56 +0200

    add methods to fighterService.js

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,45 @@
 import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
-  // TODO: Implement methods to work with fighters
+	search(search) {
+		const fighter = fighterRepository.getOne(search);
+		if (!fighter) {
+			return null;
+		}
+		return fighter;
+	}
+
+	getAll() {
+		const fighters = fighterRepository.getAll();
+		if(!fighters) {
+			return null
+		}
+		return fighters
+	}
+
+	create(data) {
+		const newFighter = fighterRepository.create(data);
+		if(!newFighter) {
+			return null
+		}
+		return newFighter
+	}
+
+	update(id, data) {
+		const updateFighter = fighterRepository.update(id, data);
+		if(!updateFighter) {
+			return null
+		}
+		return updateFighter
+	}
+
+	delete(id) {
+		const deleteFighter = fighterRepository.delete(id);
+		if(!deleteFighter) {
+			return null
+		}
+		return deleteFighter
+	}
 }
 
 const fighterService = new FighterService();
