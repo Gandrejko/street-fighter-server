bd3b31b90eadaacaaea91218b66bd2b6db43fda7
Andrew <gandrejko2002@gmail.com>
2023-01-23 14:37:09 +0200

    add all requests to fighterRoutes.js

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -8,6 +8,61 @@ import {
 
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+router.post('/', createFighterValid, (req, res, next) => {
+  try {
+    const {body, body: {name}} = req;
+
+    if(fighterService.search({name})) {
+      throw new Error(`User with name ${name} already exist`)
+    }
+
+    res.data = fighterService.create(body);
+    next();
+  } catch ({ message }) {
+    next({message, status: 400})
+  }
+}, responseMiddleware);
+
+router.get('/:id', (req, res, next) => {
+  try {
+    const { params: {id} }  = req;
+    const fighter = fighterService.search({id});
+
+    if(!fighter) {
+      throw new Error('Fighter not found');
+    }
+
+    res.data = fighter;
+    next();
+  } catch ({ message }) {
+    next({message, status: 404})
+  }
+}, responseMiddleware);
+
+router.get('/', (req, res, next) => {
+  res.data = fighterService.getAll();
+  next()
+}, responseMiddleware);
+
+router.delete('/:id', (req, res, next) => {
+  const { params: { id }}  = req;
+  res.data = fighterService.delete(id);
+  next();
+}, responseMiddleware);
+
+router.put('/:id', updateFighterValid, (req, res, next) => {
+  try {
+    const { body, params: { id }} = req;
+
+    if (!fighterService.search({id})) {
+      throw new Error('Fighter not found');
+    }
+
+    res.data = fighterService.update(id, body);
+    next();
+  } catch ({ message }) {
+    next({message, status: 400});
+  }
+}, responseMiddleware);
 
 export { router };
