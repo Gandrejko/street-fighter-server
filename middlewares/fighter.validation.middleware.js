773491b4bafe4c2e30b7a7143cf72b51db17cb08
Andrew <gandrejko2002@gmail.com>
2023-01-23 14:38:35 +0200

    add all validate

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,13 +1,73 @@
 import { FIGHTER } from "../models/fighter.js";
 
 const createFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for FIGHTER entity during creation
-  next();
-};
+  try {
+    isBodyHaveAllRequired(req);
+    validateFields(req);
+
+    next();
+  } catch ({message}) {
+    next({message, status: 400})
+  }
+}
+
+const isBodyHaveAllRequired = ({ body }) => {
+  const { id, health: fighterHealth, ...required } = body;
+  const { health: bodyHealth, ...requiredBody } = body;
+
+  const requiredKeys = JSON.stringify(Object.keys(required).sort());
+  const bodyKeys = JSON.stringify(Object.keys(requiredBody).sort());
+  if(requiredKeys != bodyKeys) {
+    throw new Error("Incorrect body fields");
+  }
+}
+
+const validateFields = ({ body }) => {
+  validationName(body);
+
+  const { health, power, defense } = body;
+
+  const configFighterParams = [
+    {value: health, min: 80, max: 120, paramName: 'Health'},
+    {value: power, min: 1, max: 100, paramName: 'Power'},
+    {value: defense, min: 1, max: 10, paramName: 'Defense'}
+  ];
+  validationFighterParams(configFighterParams);
+}
+
+const validationName = ({ name }) => {
+  if (name && name.replace(/\s/g, '').lenght < 1) {
+    throw new Error('Name cannot be empty or less than one letter');
+  }
+}
+
+const validationFighterParams = (arr) => {
+  arr.forEach(({ value, min, max, paramName }) => {
+    if ((value || value == 0) && (value < min || value > max || isNaN(value))) {
+      throw new Error(`${paramName} must be number between ${min} and ${max}`);
+    }});
+}
 
 const updateFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for FIGHTER entity during update
-  next();
-};
+  try {
+    isBodyHaveOneRequired(req);
+    validateFields(req);
+    next();
+  } catch ({message}) {
+    next({message, status: 400})
+  }
+}
+
+const isBodyHaveOneRequired = ( { body }) => {
+  const {id, ...possible} = body;
+
+  if (!Object.keys(body).every(key => possible.hasOwnProperty(key))) {
+    throw new Error('Incorrect body fields');
+  }
+
+  if(Object.keys(body).length == 0) {
+    throw new Error('Body can`t be empty')
+  }
+}
 
 export { createFighterValid, updateFighterValid };
